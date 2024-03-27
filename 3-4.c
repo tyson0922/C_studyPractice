655d946877147be2b497b5e99f759c5cc6dd6fb2
tyson0922 <55447041+tyson0922@users.noreply.github.com>
2024-03-27 19:25:30 +0900

    Add files via upload

---
+++ 3-4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("%d / %d = %f \n ", 100, 200, 0.5);
+	printf("%c %c \n", 'a', 'K');
+	printf("%s %s \n", "¾È³ç", "C ¾ð¾î");
+
+}
---
+++ 3-6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("\n줄바꿈\n연습 \n");
+	printf("\t텝키\t 연습 \n");
+	printf("이것을\r덮어씁니다 \n");
+	printf("\a삐소리 1번 \n");
+	printf("글자가 \"강조\"되는 효과 \n");
+	printf("\\\\\\ 억슬래쉬 세개 출력 \n");
+}
+
