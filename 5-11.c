1483e16f2acb4eb587a87d89cc54f733eb41dd09
tyson0922 <55447041+tyson0922@users.noreply.github.com>
2024-04-12 16:06:57 +0900

    Add files via upload

--- 5-11.c
+++ 5-11.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 void main()
 {
-int a, b;
-char c;
-printf("수식을 한줄로 띄어쓰기로 입력하세요 : ");
-scanf("%d%c%d", &a, c, &b);
-printf("%d%s%d", a, b, c);
+	int a, b, result;
+	char c;
+	printf("수식을 한줄로 띄어쓰기로 입력하세요 : ");
+	scanf("%d %c %d", &a, &c, &b);
+
+	switch (c)
+	{
+	case '-':
+		result = a - b;
+		printf("%d - %d = %d", a, b, result);
+		break;
+
+	case '+':
+		result = a + b;
+		printf("%d + %d = %d", a, b, result);
+		break;
+	case '*':
+		result = a * b;
+		printf("%d * %d = %d", a, b, result);
+		break;
+	case '/':
+		result = a / b;
+		printf("%d / %d = %d", a, b, result);
+		break;
+	case '%':
+		result = a % b;
+		printf("%d %% %d = %d", a, b, result);
+		break;
+	default :
+		printf("연산자를 잘못 입력했습니다.");
+	}
 }
---
+++ 6-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+	int i;
+
+	for(i = 0; i<5 ; i++)
+	{
+		printf("안녕하세요? 방금방금 for 문을 공부중입니다. ^^\n");
+	}
+}
