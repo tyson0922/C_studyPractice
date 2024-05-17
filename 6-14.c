c7e83f3eec0377068cc522c5db49626c11e22706
tyson0922 <55447041+tyson0922@users.noreply.github.com>
2024-05-17 15:25:45 +0900

    Add files via upload

---
+++ 6-14.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+	int i, k;
+	for (i = 1; i <= 9; i++)
+	{
+		for (k = 2; k <= 9; k++)
+		{
+			printf("%2dX%2d=%2d", k, i, k*i);
+		}
+		{
+		printf("\n");
+		}	
+	}
+}
+
+
